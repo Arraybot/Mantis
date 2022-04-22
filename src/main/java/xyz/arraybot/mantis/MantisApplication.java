0cbb54c0f57baadf04439ee813d95bd4a1be9d49
Arraying <paul.huebner@googlemail.com>
2022-04-22 18:07:08 +0200

    Create Spring Initializr template.

---
+++ src/main/java/xyz/arraybot/mantis/MantisApplication.java
@@ -0,0 +1,13 @@
+package xyz.arraybot.mantis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantisApplication.class, args);
+	}
+
+}
